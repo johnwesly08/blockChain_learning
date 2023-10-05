c2b7ed91e2ccb4e657e1f8b60b441692d6ee23fb
johnwesly08 <johnwesleywork08@gmail.com>
2023-10-05 16:54:00 +0530

    Functions completed

--- contracts/Basics_one.sol
+++ contracts/Basics_one.sol
@@ -1,7 +1,59 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity >=0.7.0 <0.9.0;
 
-
 contract intro {
     string public str="Hello World";//printing a hello world
 }
+
+// //declaring a state variable
+// //uint => Unsigned Integer
+
+// contract variable {
+//     // uint public num = 5;//state variable
+
+//     // constructor() {
+//     //     num = 10;
+//     // }
+
+//     /*Always declaring any variable or function along the Public keyword will make those functionality to be open and public */
+//     /*Generally all the variables that is declared inside the contract outside the function is said to be the State Variable. Meanwhile, this state variable will be usually in the permanent memory like storage in storage contract */
+    
+//     uint public num;// By declaring a variable with no value will gives the output value as Zero(Which is a default)
+
+//     //declaring a function
+//     function seter() public {
+//         num = 200;
+//     }
+// }
+// // Thus, to change the dafault values of the state variable, we can use the above methods => 1. constructor, 2.Setter Function,3. Or by just change the initializing of a variable
+
+// contract _variable {
+
+//     uint c;//State variable
+//     function local() public pure returns(uint) {
+//         uint a;//Local Variable
+//         uint b;//Local Variable
+//         return a+b;
+//     }/*There are actually 3 areas namely, 1. Stack, 2. Storage, 3. Memory */
+// }
+
+// how to create a function
+contract func_1 {
+    uint public num1; //State variable
+    uint abc;// it is not a public variable
+
+    function setter(uint _num) public {
+        num1 = _num;
+    }
+
+    //We are reading the state variable, and that's why we have used the {view {Keyword}}
+    function getter() public view returns(uint){
+        return num1;
+    }
+}
+
+//important points for functions
+/*
+    when setter function is called and used, it creates a t4ransaction and costs a gas, becoz it changes the blockChain
+    Whenever you're declaring a public state variable, a getter function will be generated automatically.
+*/
--- contracts/Basics_two.sol
+++ contracts/Basics_two.sol
@@ -2,26 +2,3 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 < 0.9.0;
 
-//declaring a state variable
-
-//uint => Unsigned Integer
-
-contract variable {
-    // uint public num = 5;//state variable
-
-    // constructor() {
-    //     num = 10;
-    // } // This actually updates the variable
-
-    //declaring a function
-
-    /*Always declaring any variable or function along the Public keyword will make those functionality to be open and public */
-    
-    
-    uint public num;// By declaring a variable with no value will gives the output value as Zero(Which is a default)
-
-    function seter() public {
-        num = 200;
-    }
-
-}
